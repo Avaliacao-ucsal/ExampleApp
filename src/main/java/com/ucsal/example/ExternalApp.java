4f9e5f8dd5826930a1d4a04389771b32c9d7c32e
atila.macedo <atila.macedo@medkey.com>
2024-05-30 00:38:34 -0300

    commit do app de exemplo

---
+++ src/main/java/com/ucsal/example/ExternalApp.java
@@ -0,0 +1,19 @@
+package com.ucsal.example;
+
+import com.ucsal.notification.channels.EmailNotification;
+import com.ucsal.notification.channels.WhatsAppNotification;
+import com.ucsal.notification.models.Notification;
+
+public class ExternalApp {
+	public static void main(String[] args) {
+        // Exemplo de uso do EmailNotification
+        EmailNotification emailNotification = new EmailNotification("smtp.example.host", "user","password");
+        Notification notification = new Notification("seu@email", "Subject", "Message");
+        emailNotification.send(notification);
+
+        // Exemplo de uso do WhatsAppNotification
+        WhatsAppNotification whatsAppNotification = new WhatsAppNotification();
+        Notification notificationWpp = new Notification("number", "Message","desgraça");
+        whatsAppNotification.send(notificationWpp);
+	}
+}
